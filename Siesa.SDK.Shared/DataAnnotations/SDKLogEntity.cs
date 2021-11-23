5de81c0cee25e1fee9576d703762a0d87d5a6eca
Jonathan Toledo <kry0ster@gmail.com>
2021-11-22 20:03:17 -0500

    wip: data annotation

---
+++ Siesa.SDK.Shared/DataAnnotations/SDKLogEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siesa.SDK.Shared.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SDKLogEntity: Attribute 
+    {
+        //List of fields
+        public string[] Fields { get; set; }
+
+        //Constructor
+        public SDKLogEntity(string[] fields)
+        {
+            Fields = fields;
+        }
+
+        public SDKLogEntity()
+        {
+            Fields = new string[] { };
+        }
+        
+    }
+}
