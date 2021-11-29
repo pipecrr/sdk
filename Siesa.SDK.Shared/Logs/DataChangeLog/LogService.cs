c4e133e091c3c9042937ce6c9fb4ffdb9a1b29d6
Rodolfo Reina Figueroa <rodolfo.reina@siesa.com>
2021-11-29 16:14:09 -0500

    clean comments

--- Siesa.SDK.Shared/Logs/DataChangeLog/LogService.cs
+++ Siesa.SDK.Shared/Logs/DataChangeLog/LogService.cs
@@ -10,13 +10,7 @@ namespace Siesa.SDK.Shared.Logs.DataChangeLog
         public static void SaveDataEntityLog(List<DataEntityLog> logs)
         {
             var dataChangeLog = new SDKGrpcChangeLogStorageService("https://localhost:7168");
-            //var tasks = new List<Task>();
             Parallel.ForEach(logs, log => ConvertAndSave(log, dataChangeLog));
-            //foreach (var log in logs) { 
-            //    string result = JsonConvert.SerializeObject(log);
-            //    tasks.Add(dataChangeLog.Save(result));
-            //}
-            //await Task.WhenAll(tasks);
         }
 
         private static void ConvertAndSave(DataEntityLog log, SDKGrpcChangeLogStorageService dataChangeLog)
