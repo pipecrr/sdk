35ce3a0b46cbd905fc33297ce9431c5ce306078e
Jonathan Toledo <jtoledo@siesa.com>
2022-02-23 17:49:42 -0500

    Default JSONContractResolver

---
+++ Siesa.SDK.Shared/Json/SDKContractResolver.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Siesa.SDK.Shared.Json
+{
+    public class SDKContractResolver : DefaultContractResolver
+{
+    public new static readonly SDKContractResolver Instance = new SDKContractResolver();
+
+    protected override JsonContract CreateContract(Type objectType)
+    {
+        JsonContract contract = base.CreateContract(objectType);
+
+        // this will only be called once and then cached
+        if (objectType == typeof(DateOnly))
+        {
+            contract.Converter = new DateOnlyJsonConverter();
+        }
+
+        return contract;
+    }
+}
+
+}
