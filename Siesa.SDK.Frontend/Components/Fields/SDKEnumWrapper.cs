58ecb699b92d6160a1ae4dfa7a83f8c4debc634c
Jonathan Toledo <jtoledo@siesa.com>
2022-03-04 16:35:01 -0500

    selectfield

---
+++ Siesa.SDK.Frontend/Components/Fields/SDKEnumWrapper.cs
@@ -0,0 +1,7 @@
+namespace Siesa.SDK.Frontend.Components.Fields
+{
+    public class SDKEnumWrapper<T> {
+        public T Type { get; set; }
+        public string DisplayText { get; set; }
+    }
+}
