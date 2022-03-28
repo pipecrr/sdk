dd27e37736d5a21eaf764b5484d10840479bec99
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-03-28 12:33:44 -0500

    REGENERACION E00021_Culture

--- Siesa.SDK.Shared/Entities/E00021_Culture.cs
+++ Siesa.SDK.Shared/Entities/E00021_Culture.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Siesa.SDK.Shared.Json;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace Siesa.SDK.Entities
