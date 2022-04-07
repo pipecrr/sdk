0b22356a2861970eff8d88ef918a52691982865f
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-04-07 09:06:34 -0500

    CREACION E00023_ResourceCustomDescription

---
+++ Siesa.SDK.Shared/Entities/E00023_ResourceCustomDescription.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json;
+using Siesa.SDK.Entities;
+using Siesa.SDK.Shared.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Siesa.SDK.Shared.Json;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Siesa.SDK.Entities
+{
+	/// <summary>
+	/// Recursos personalizados
+	/// </summary>
+	[SDKLogEntity]
+	[Index(nameof(RowidResourceDescription), nameof(Description), Name = "IX_e00023_1")]
+	public class E00023_ResourceCustomDescription : BaseEntity<int>
+	{
+		[Key]
+		[Required]
+		public override int Rowid { get; set; }
+
+		[ForeignKey("ResourceDescription")]
+		[Required]
+		public  int RowidResourceDescription { get; set; }
+
+		[Required]
+		[StringLength(2000)]
+		public  string Description { get; set; }
+
+
+		[Required]
+		public E00022_ResourceDescription ResourceDescription { get; set; }
+
+	}
+}
