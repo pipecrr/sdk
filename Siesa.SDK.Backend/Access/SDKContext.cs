348695ee8b6bcc19b48b775fad4c40c65ece9df9
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-04-08 15:51:40 -0500

    CREACION E00042_Action

--- Siesa.SDK.Backend/Access/SDKContext.cs
+++ Siesa.SDK.Backend/Access/SDKContext.cs
@@ -17,6 +17,8 @@ namespace Siesa.SDK.Backend.Access
 {
     public abstract class SDKContext: DbContext
     {
+	public DbSet<E00042_Action>? E00042_Action { get; set; }
+
 	public DbSet<E00041_ModuleFeature>? E00041_ModuleFeature { get; set; }
 
 	public DbSet<E00040_Feature>? E00040_Feature { get; set; }
