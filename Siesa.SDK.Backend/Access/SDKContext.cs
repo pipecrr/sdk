7de158713941eab4cb485b0ffbf2150cd1e45a16
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-04-21 14:27:34 -0500

    CREACION E00067_SuiteMenuCustom

--- Siesa.SDK.Backend/Access/SDKContext.cs
+++ Siesa.SDK.Backend/Access/SDKContext.cs
@@ -17,6 +17,8 @@ namespace Siesa.SDK.Backend.Access
 {
     public abstract class SDKContext: DbContext
     {
+	public DbSet<E00067_SuiteMenuCustom>? E00067_SuiteMenuCustom { get; set; }
+
 	public DbSet<E00025_GenericEnumValueModule>? E00025_GenericEnumValueModule { get; set; }
 
 	public DbSet<E00024_GenericEnumValue>? E00024_GenericEnumValue { get; set; }
---
+++ Siesa.SDK.Shared/Entities/E00067_SuiteMenuCustom.cs
@@ -0,0 +1,39 @@
+using Newtonsoft.Json;
+using Siesa.SDK.Entities;
+using Siesa.SDK.Shared.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Siesa.SDK.Shared.Json;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Siesa.SDK.Entities
+{
+	/// <summary>
+	/// Suite Menú personalizado 
+	/// </summary>
+	[SDKLogEntity]
+	public class E00067_SuiteMenuCustom : BaseAudit<int>
+	{
+		[Key]
+		[Required]
+		public override int Rowid { get; set; }
+
+		[Required]
+		public short RowidSuiteCustom { get; set; }
+
+		public int? RowidMenuCustom { get; set; }
+
+		public int? RowidMenu { get; set; }
+
+		[Required]
+		public byte Order { get; set; }
+
+		[Required]
+		public bool IsPrivate { get; set; }
+
+
+	}
+}
