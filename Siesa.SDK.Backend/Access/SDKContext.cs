0815a652f35e433023e3929a2de3f212ccf21676
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-04-21 14:23:26 -0500

    CREACION E00025_GenericEnumValueModule

--- Siesa.SDK.Backend/Access/SDKContext.cs
+++ Siesa.SDK.Backend/Access/SDKContext.cs
@@ -17,6 +17,8 @@ namespace Siesa.SDK.Backend.Access
 {
     public abstract class SDKContext: DbContext
     {
+	public DbSet<E00025_GenericEnumValueModule>? E00025_GenericEnumValueModule { get; set; }
+
 	public DbSet<E00024_GenericEnumValue>? E00024_GenericEnumValue { get; set; }
 
 	public DbSet<E00023_ResourceCustomDescription>? E00023_ResourceCustomDescription { get; set; }
---
+++ Siesa.SDK.Shared/Entities/E00025_GenericEnumValueModule.cs
@@ -0,0 +1,33 @@
+using Newtonsoft.Json;
+using Siesa.SDK.Entities;
+using Siesa.SDK.Shared.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Siesa.SDK.Shared.Json;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Siesa.SDK.Entities
+{
+	/// <summary>
+	/// Módulo  enum valor genérico
+	/// </summary>
+
+	[Index(nameof(RowidGenericEnumValue), nameof(RowidModule), Name = "IX_e00025_1", IsUnique = true)]
+	public class E00025_GenericEnumValueModule : BaseSDK<int>
+	{
+		[Key]
+		[Required]
+		public override int Rowid { get; set; }
+
+		[Required]
+		public int RowidGenericEnumValue { get; set; }
+
+		[Required]
+		public short RowidModule { get; set; }
+
+
+	}
+}
