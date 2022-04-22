4bf0768e1fa88e9adc9a6abdd36da6e05d19ee2f
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-04-22 11:12:21 -0500

    ELIMINACION U00066_MenuCustom

--- Siesa.SDK.Backend/Access/SDKContext.cs
+++ Siesa.SDK.Backend/Access/SDKContext.cs
@@ -35,8 +35,6 @@ namespace Siesa.SDK.Backend.Access
 
 	public DbSet<E00042_Operation>? E00042_Operation { get; set; }
 
-	public DbSet<U00066_MenuCustom>? U00066_MenuCustom { get; set; }
-
 	public DbSet<E00041_Action>? E00041_Action { get; set; }
 
 	public DbSet<E00066_MenuCustom>? E00066_MenuCustom { get; set; }
