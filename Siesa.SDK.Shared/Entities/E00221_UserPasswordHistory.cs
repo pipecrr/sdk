3ddcd731e31a5e1d3435e28d819b6adbab279808
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-04-28 11:25:12 -0500

    CREACION E00221_UserPasswordHistory

---
+++ Siesa.SDK.Shared/Entities/E00221_UserPasswordHistory.cs
@@ -0,0 +1,43 @@
+using Newtonsoft.Json;
+using Siesa.SDK.Entities;
+using Siesa.SDK.Shared.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Siesa.SDK.Shared.Json;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Siesa.SDK.Entities
+{
+	/// <summary>
+	/// Historial de contraseñas por usuario
+	/// </summary>
+
+	[Index(nameof(RowidUser), nameof(PasswordSequence), Name = "IX_e00221_1", IsUnique = true)]
+	public class E00221_UserPasswordHistory : BaseSDK<int>
+	{
+		[Key]
+		[Required]
+		public override int Rowid { get; set; }
+
+		[Required]
+		public int RowidUser { get; set; }
+
+		[Required]
+		public short PasswordSequence { get; set; }
+
+		[Required]
+		[StringLength(128)]
+		public string Password { get; set; }
+
+		[Required]
+		public DateTime PasswordAssignmentDate { get; set; }
+
+		[Required]
+		public DateTime PasswordLastUpdate { get; set; }
+
+
+	}
+}
