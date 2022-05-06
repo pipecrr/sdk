21ab94bb25ce6b7ed4e2ef0aa77b17f43a44859e
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-05-06 17:42:18 -0500

    MODIFICACION E00025_GenericEnumValueModule

--- Siesa.SDK.Shared/Entities/E00025_GenericEnumValueModule.cs
+++ Siesa.SDK.Shared/Entities/E00025_GenericEnumValueModule.cs
@@ -18,6 +18,7 @@ namespace Siesa.SDK.Entities
 	[Index(nameof(RowidGenericEnumValue), nameof(RowidModule), Name = "IX_e00025_1", IsUnique = true)]
 	public class E00025_GenericEnumValueModule : BaseSDK<int>
 	{
+		[SDKIdentity]
 		[Key]
 		[Required]
 		public override int Rowid { get; set; }
