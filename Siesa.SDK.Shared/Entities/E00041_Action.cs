29f03de42e24aacd3e1a8da7a1319ad2b0f5cf47
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-05-06 17:42:41 -0500

    MODIFICACION E00041_Action

--- Siesa.SDK.Shared/Entities/E00041_Action.cs
+++ Siesa.SDK.Shared/Entities/E00041_Action.cs
@@ -18,6 +18,7 @@ namespace Siesa.SDK.Entities
 	[Index(nameof(RowidResource), Name = "IX_e00041_1", IsUnique = true)]
 	public class E00041_Action : BaseSDK<int>
 	{
+		[SDKIdentity]
 		[Key]
 		[Required]
 		public override int Rowid { get; set; }
