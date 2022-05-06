dc408d9fe0148b4ba2933ba30fe66f2811e9cdcb
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-05-06 17:41:41 -0500

    MODIFICACION E00023_ResourceCustomDescription

--- Siesa.SDK.Shared/Entities/E00023_ResourceCustomDescription.cs
+++ Siesa.SDK.Shared/Entities/E00023_ResourceCustomDescription.cs
@@ -18,6 +18,7 @@ namespace Siesa.SDK.Entities
 	[Index(nameof(RowidResourceDescription), Name = "IX_e00023_1", IsUnique = true)]
 	public class E00023_ResourceCustomDescription : BaseAudit<int>
 	{
+		[SDKIdentity]
 		[Key]
 		[Required]
 		public override int Rowid { get; set; }
