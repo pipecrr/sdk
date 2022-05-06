f93c44caa1700ac549692189d7116c3e233b6080
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-05-06 17:24:29 -0500

    MODIFICACION E00225_UserDataVisibilityGroup

--- Siesa.SDK.Shared/Entities/E00225_UserDataVisibilityGroup.cs
+++ Siesa.SDK.Shared/Entities/E00225_UserDataVisibilityGroup.cs
@@ -14,7 +14,7 @@ namespace Siesa.SDK.Entities
 	/// <summary>
 	/// Grupos
 	/// </summary>
-
+	[SDKLogEntity]
 	[Index(nameof(RowidDataVisibilityGroup), nameof(RowidUser), Name = "IX_e00225_1", IsUnique = true)]
 	public class E00225_UserDataVisibilityGroup : BaseAudit<int>
 	{
