9a7e035d97f7a8be0092c9fa04b5d962b78bc678
Jonathan Toledo <jtoledo@siesa.com>
2022-06-23 21:43:08 -0500

    Header & Menu

--- Siesa.SDK.Shared/Entities/E00061_Menu.cs
+++ Siesa.SDK.Shared/Entities/E00061_Menu.cs
@@ -48,5 +48,22 @@ namespace Siesa.SDK.Entities
         public virtual ICollection<E00061_Menu> SubMenus { get; set; }
         [NotMapped]
         public virtual string ResourceTag { get; set; }
+        [NotMapped]
+        public virtual string CurrentText { get; set; }
+
+        [NotMapped]
+        public virtual string CurrentURL { get{
+            var url = "";
+            if (!string.IsNullOrEmpty(this.Url)) {
+                url = this.Url;
+            }else{
+                if(this.Feature != null){
+                    url = $"/{this.Feature.BusinessName}/";
+                }else{
+                    url = "/";
+                }
+            }
+            return url;
+        } }
     }
 }
