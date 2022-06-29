d64f35831615cb9b869ea32138ce1b2536845180
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-06-29 17:59:22 -0500

    REGENERACION E00021_Culture

--- Siesa.SDK.Shared/Entities/E00021_Culture.cs
+++ Siesa.SDK.Shared/Entities/E00021_Culture.cs
@@ -16,7 +16,7 @@ namespace Siesa.SDK.Entities
 	/// </summary>
 
 	[Index(nameof(LanguageCode), nameof(CountryCode), Name = "IX_e00021_1", IsUnique = true)]
-	public class E00021_Culture : BaseSDK<short>
+	public partial class E00021_Culture : BaseSDK<short>
 	{
 		[SDKIdentity]
 		[Key]
@@ -34,10 +34,6 @@ namespace Siesa.SDK.Entities
 		[StringLength(500)]
 		public string Description { get; set; }
 
-		public override string ToString()
-		{
-			return $"({LanguageCode}{(CountryCode != null ? '_'+CountryCode : CountryCode)}) - {Description}";
-		}
 
 	}
 }
