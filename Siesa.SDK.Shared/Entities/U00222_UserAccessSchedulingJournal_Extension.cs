7c57ba9e28729a73fab70515f23d50a0da6e6db5
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-06-29 17:42:08 -0500

    Regeneración de entidades proyecto SDK.

---
+++ Siesa.SDK.Shared/Entities/U00222_UserAccessSchedulingJournal_Extension.cs
@@ -0,0 +1,20 @@
+using Newtonsoft.Json;
+using Siesa.SDK.Entities;
+using Siesa.SDK.Shared.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Siesa.SDK.Shared.Json;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Siesa.SDK.Entities
+{
+	public partial class U00222_UserAccessSchedulingJournal
+	{
+
+
+
+	}
+}
