24b8951970a88b196666b6c37e42fa8ec9179f1e
AWS-SRV-GTABLAS$ <AWS-SRV-GTABLAS$@siesa.com>
2022-06-29 16:56:19 -0500

    Regeneración de entidades proyecto SDK.

--- Siesa.SDK.Shared/Entities/BaseCompanySummary_Extension.cs
+++ Siesa.SDK.Shared/Entities/BaseCompanySummary_Extension.cs
@@ -4,15 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Siesa.SDK.Entities
 {
-
 	public abstract partial class BaseCompanySummary : BaseSDK<int>
 	{
-		[Key]
-		[Required]
-		public override int Rowid { get; set; }
-
-		[Required]
-		public virtual short RowidCompany { get; set; }
 
 
 	}
---
+++ Siesa.SDK.Shared/Entities/BaseMaster_Extension.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Siesa.SDK.Entities
+{
+	public abstract partial class BaseMaster<T> : BaseAudit<int>
+	{
+
+
+	}
+}
