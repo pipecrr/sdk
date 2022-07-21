cd8b812121f51650b7bda7bc3976df808a8f2ff0
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2022-07-21 17:39:25 -0500

    Ajustes Componente SelectBar

--- Siesa.SDK.Frontend/Components/Fields/SDKSelectBar.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKSelectBar.razor.cs
@@ -4,17 +4,15 @@ using System.Collections.Generic;
 
 namespace Siesa.SDK.Frontend.Components.Fields
 {
-    public class SelectBarItemWrap
-    {
-        public int Key { get; set; }
-        public string Name { get; set; }
-    }
     public partial class SDKSelectBar : ComponentBase
     {
 
-        [Parameter]
-        public IEnumerable<SelectBarItemWrap> Data { get; set; }
+        [Parameter] public IEnumerable<SelectBarItemWrap> Data { get; set; }
+
         //bool singleValue = false;
-        int selectedValue = 0;
+
+        [Parameter] public int Value { get; set; }
+
+        [Parameter] public EventCallback<int> ValueChanged {get; set;}
     }
 }
---
+++ Siesa.SDK.Frontend/Components/Fields/SelectBarItemWrap.cs
@@ -0,0 +1,8 @@
+namespace Siesa.SDK.Frontend.Components
+{
+    public class SelectBarItemWrap
+    {
+        public int Key { get; set; }
+        public string Name { get; set; }
+    }
+}
