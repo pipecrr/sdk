285c88abc618f07a13cf053ac95adfd0123c82cf
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2022-07-25 15:18:33 -0500

    Ajustes Select Bar

--- Siesa.SDK.Frontend/Components/Fields/SDKSelectBar.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKSelectBar.razor.cs
@@ -10,8 +10,6 @@ namespace Siesa.SDK.Frontend.Components.Fields
 
         [Parameter] public IEnumerable<SelectBarItemWrap> Data { get; set; }
 
-        //bool singleValue = false;
-
         [Parameter] public int Value { get; set; }
 
         [Parameter] public Action<int> ValueChanged {get; set;}
