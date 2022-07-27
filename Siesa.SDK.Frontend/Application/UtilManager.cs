6db825737a7f03409d7c28f6eb8e1408a9227b31
arnol.meneses <arnol.meneses@siesa.com>
2022-07-27 09:21:59 -0500

    * Se grea servicio UtilManager para el manejo de utilidades generales   como servicio * Se agrega el servicio en Sturtup como Scoped * Se agrega consumo de servicios en Componente Step

---
+++ Siesa.SDK.Frontend/Application/UtilManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Siesa.SDK.Frontend.Application;
+using Siesa.SDK.Shared.Services;
+
+namespace Siesa.SDK.Frontend.Components.FormManager.Model
+{
+    public class UtilsManager
+    {
+        private IAuthenticationService AuthenticationService;
+        private IResourceManager ResourceManager;
+        public UtilsManager(IAuthenticationService authenticationService, IResourceManager resourceManager)
+        {
+            this.AuthenticationService = authenticationService;
+            this.ResourceManager = resourceManager;
+        }
+
+        public async Task<string> GetResource(Int64 resourceRowid){
+            if(AuthenticationService.User != null){
+                Int64 cultureRowid = AuthenticationService.User.RowidCulture;
+                return await ResourceManager.GetResource(resourceRowid, cultureRowid);
+            }
+            return "";
+        }
+
+        public async Task<string> GetResource(string resourceTag){
+
+            var t = AuthenticationService.User;
+            if(AuthenticationService.User != null){
+                Int64 cultureRowid = AuthenticationService.User.RowidCulture;
+                return await ResourceManager.GetResource(resourceTag, cultureRowid);
+            }
+            return resourceTag;
+        }
+    }
+}
