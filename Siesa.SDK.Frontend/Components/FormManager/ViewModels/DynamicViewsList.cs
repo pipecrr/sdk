87a5ecce0f720daff99955e2ecef9a31c169d0ab
Jonathan Toledo <jtoledo@siesa.com>
2022-08-02 08:29:49 -0500

    OnReady

---
+++ Siesa.SDK.Frontend/Components/FormManager/ViewModels/DynamicViewsList.cs
@@ -0,0 +1,12 @@
+namespace Siesa.SDK.Frontend.Components.FormManager.ViewModels
+{
+    public enum DynamicViewType
+    {
+        Detail,
+        Create,
+        Edit,
+        List,
+        Subpanel,
+        Custom
+    }
+}
