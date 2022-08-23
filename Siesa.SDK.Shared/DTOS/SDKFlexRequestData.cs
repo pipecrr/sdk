e63cb7a8fa6cf0fa31d7b106ec97b2d1cd82f601
arnol.meneses <arnol.meneses@siesa.com>
2022-08-23 16:17:10 -0500

    sdkFlex
    * Se agrega filtros dinamicamente
    * se traen datos de las entidades

--- Siesa.SDK.Shared/DTOS/SDKFlexRequestData.cs
+++ Siesa.SDK.Shared/DTOS/SDKFlexRequestData.cs
@@ -6,7 +6,7 @@ namespace Siesa.SDK.Shared.DTOS
     {
         public string module_path { get; set; }
         public string selected_class { get; set; }
-        public List<object> filters { get; set; }
+        public List<SDKFlexFilters> filters { get; set; }
         public List<SDKFlexColumn> columns { get; set; }
         public List<object> groups { get; set; }
     }
