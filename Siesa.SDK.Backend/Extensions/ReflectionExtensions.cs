549219b6f1140f8a9821f7809e3aef2e2bb0acf9
Jonathan Toledo <jtoledo@siesa.com>
2022-09-13 17:49:45 -0500

    merge flex

---
+++ Siesa.SDK.Backend/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Siesa.SDK.Backend.Extensions
+{
+    public static class ReflectionExtensions
+	{   
+		public static IEnumerable<MethodInfo> GetExtensionMethods(this Type type, Assembly extensionsAssembly)
+		{
+			var query = from t in extensionsAssembly.GetTypes()
+						where !t.IsGenericType && !t.IsNested
+						from m in t.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+						where m.IsDefined(typeof(System.Runtime.CompilerServices.ExtensionAttribute), false)
+						where m.GetParameters()[0].ParameterType.Name == type.Name
+						select m;
+
+			return query;
+		}
+
+		public static MethodInfo GetExtensionMethod(this Type type, Assembly extensionsAssembly, string name)
+		{
+			return type.GetExtensionMethods(extensionsAssembly).FirstOrDefault(m => m.Name == name);
+		}
+
+		public static MethodInfo GetExtensionMethod(this Type type, Assembly extensionsAssembly, string name, Type[] types)
+		{
+			var methods = (from m in type.GetExtensionMethods(extensionsAssembly)
+						where m.Name == name
+						&& m.GetParameters().Count() == types.Length
+						select m).ToList();
+
+			if (!methods.Any())
+			{
+				return default(MethodInfo);
+			}
+
+			if (methods.Count() == 1)
+			{
+				return methods.First();
+			}
+
+			foreach (var methodInfo in methods)
+			{
+				var parameters = methodInfo.GetParameters();
+
+				bool found = true;
+				for (byte b = 0; b < types.Length; b++)
+				{
+					found = true;
+					if (parameters[b].ParameterType != types[b] || (types[b].IsGenericType && types[b].Name != parameters[b].ParameterType.Name))
+					{
+						found = false;
+					}
+				}
+
+				if (found)
+				{
+					return methodInfo;
+				}
+			}
+
+			return default(MethodInfo);
+		}
+	} 
+}
---
+++ Siesa.SDK.Shared/DTOS/SDKFlexRequestSaveData.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace Siesa.SDK.Shared.DTOS
+{
+    public class SDKFlexRequestSaveData
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string selected_class { get; set; }
+        public List<object> visible_users { get; set; }
+        public List<object> visible_roles { get; set; }
+        public string module_path { get; set; }
+        public List<SDKFlexColumn> select_list { get; set; }
+        public List<SDKFlexFilters> filter_list { get; set; }
+        public List<object> chart_list { get; set; }
+        public Owner owner { get; set; }
+        public OwnerTeam owner_team { get; set; }
+        public List<object> group_list { get; set; }
+        public int version { get; set; }
+        public string metadata { get; set; }
+    }
+
+    public class Owner
+    {
+        public string id { get; set; }
+    }
+
+    public class OwnerTeam
+    {
+    }
+}
