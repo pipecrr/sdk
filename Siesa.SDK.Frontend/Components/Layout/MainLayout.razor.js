7a78e862b3488ab67dc3e4ffb920508e72d424e2
Jonathan Toledo <jtoledo@siesa.com>
2022-09-14 10:09:14 -0500

    watermark

---
+++ Siesa.SDK.Frontend/Components/Layout/MainLayout.razor.js
@@ -0,0 +1,13 @@
+﻿export function PoblateWatermark() {
+    setTimeout(function () {
+        var clientWidth = document.body.clientWidth;
+        Array.from(document.querySelectorAll('.sdk-test-watermark')).forEach(function(el) {
+
+            var text = el.dataset.watermark;
+            var textLength = text.length;
+            var repeatTimes = Math.floor(clientWidth / textLength * 10);
+
+            el.dataset.watermark = (el.dataset.watermark + ' ').repeat(repeatTimes);
+        });
+    }, 500);
+}
