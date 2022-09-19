71ab3455eaf277d09799dcf7de7eb397ed92048d
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2022-09-19 11:07:06 -0500

    Init SDK-Docs

---
+++ Siesa.SDK.Frontend/Components/Documentation/SDKDocumentation.razor.cs
@@ -0,0 +1,28 @@
+
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Siesa.SDK.Frontend.Components.Documentation
+{
+
+public class ComponentCategory{
+     
+    public string Name {get; set;}
+    public List<ComponentDemo> Components {get; set;} = new List<ComponentDemo>();
+
+    public string Icon {get; set;}
+
+}
+public class ComponentDemo {
+
+    public string ComponentName {get; set;}
+
+    public RenderFragment ComponentFragment {get;set;}
+
+    public string URL {get; set;}
+
+    public object ID { get; set; }
+}
+    
+}
