7a1c31e8be52d6c604924104215bb617a981986c
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2022-11-03 15:03:44 -0500

    Documentacion DataAnnotations.

--- Siesa.SDK.Shared/DataAnnotations/SDKExposedMethod.cs
+++ Siesa.SDK.Shared/DataAnnotations/SDKExposedMethod.cs
@@ -5,18 +5,8 @@ namespace Siesa.SDK.Shared.DataAnnotations
     [AttributeUsage(AttributeTargets.Method)]
     public class SDKExposedMethod : Attribute
     {
-        //List of fields
-        public string TableName { get; set; }
-
-        //Constructor
-        public SDKExposedMethod(string table_name)
-        {
-            TableName = table_name;
-        }
-
         public SDKExposedMethod()
         {
-            TableName = "";
         }
     }
 }
