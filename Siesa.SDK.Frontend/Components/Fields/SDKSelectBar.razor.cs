3a882503d4f3e400da864e7cc6987947b88b8cbb
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2022-12-07 17:36:35 -0500

    Ajustes

--- Siesa.SDK.Frontend/Components/Fields/SDKSelectBar.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKSelectBar.razor.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Siesa.SDK.Frontend.Components.Fields
 {
-    public partial class SDKSelectBar<ItemType> : ComponentBase
+    public partial class SDKSelectBar<ItemType> : SDKComponent
     {
 
         [Parameter] public IEnumerable<SelectBarItemWrap<ItemType>> Data { get; set; }
@@ -15,14 +15,6 @@ namespace Siesa.SDK.Frontend.Components.Fields
         [Parameter] public Action<ItemType> ValueChanged {get; set;}
 
         [Parameter] public bool Disabled { get; set; }
-
-        [Parameter]
-        public string AutomationId { get; set; }
-
-        private string GetAutomationId()
-        {
-            return $"{this.GetType().Name}_{AutomationId}";
-        }
 
     }
 }
