4f45cc08944182b90fa2df6567310fa5c395ccb7
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2022-12-19 15:01:22 -0500

    Ajustes Tipado del Menu.

--- Siesa.SDK.Shared/Entities/Enums/SDKEnums.cs
+++ Siesa.SDK.Shared/Entities/Enums/SDKEnums.cs
@@ -5,8 +5,9 @@ namespace Siesa.SDK.Entities.Enums
 	public enum MenuType
 	{
 		Separator = 1,
-		Menu = 2,
-		Feature = 3
+		Feature = 2,
+		Compound = 3,
+		CustomMenu= 4
 	}
 
 	public enum PermissionUserTypes
