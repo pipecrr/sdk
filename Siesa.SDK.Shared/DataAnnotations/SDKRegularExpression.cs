9def92183068c5e40ed4ea3fdf3c2b8175a41350
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-01-03 12:51:57 -0500

    Ajustes DataAnnotations

---
+++ Siesa.SDK.Shared/DataAnnotations/SDKRegularExpression.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siesa.SDK.Shared.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class SDKRegularExpression: RegularExpressionAttribute 
+    {
+        public SDKRegularExpression(string pattern): base(pattern) 
+        {
+            this.ErrorMessage = "Custom.Validator.FieldFormat";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            ValidationResult validationResult = base.IsValid(value, validationContext);
+            if (validationResult == ValidationResult.Success)
+            {
+                return validationResult;
+            }
+
+            return new ValidationResult(
+                $"{validationResult.ErrorMessage}//{validationContext.ObjectType.Name}.{validationContext.MemberName}", new string[1] { validationContext.MemberName });
+        }
+    }
+}
---
+++ Siesa.SDK.Shared/DataAnnotations/SDKStringLength.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siesa.SDK.Shared.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class SDKStringLength: StringLengthAttribute 
+    {
+        public SDKStringLength(int maximumLength): base(maximumLength) 
+        {
+            this.ErrorMessage = "Custom.Validator.FieldLength";
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            ValidationResult validationResult = base.IsValid(value, validationContext);
+            if (validationResult == ValidationResult.Success)
+            {
+                return validationResult;
+            }
+
+            return new ValidationResult($"{validationResult.ErrorMessage}//{validationContext.ObjectType.Name}.{validationContext.MemberName}//{this.MaximumLength}//{this.MinimumLength}", new string[1] { validationContext.MemberName });
+        }
+    }
+}
