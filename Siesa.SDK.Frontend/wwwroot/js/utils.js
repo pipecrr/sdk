b3bf9b0c506b6be746428a66b72b7f684dcd3377
arnol.meneses <arnol.meneses@siesa.com>
2023-03-03 10:39:11 -0500

    correcciones carga flexComponent.js mejoras rendimiento flexComponent

--- Siesa.SDK.Frontend/wwwroot/js/utils.js
+++ Siesa.SDK.Frontend/wwwroot/js/utils.js
@@ -73,6 +73,7 @@ function preloadFlex(){
     var sdk_debug = params.find(x => x == 'sdk_debug=1');
     //sdk_debug = true;
     if(sdk_debug){
+        loadScript('/_content/Siesa.SDK.Frontend/flex/FlexComponent.debug.js');
         loadScript("http://127.0.0.1:3000/static/js/bundle.js");
         loadScript("http://127.0.0.1:3000/static/js/0.chunk.js");
         loadScript("http://127.0.0.1:3000/static/js/1.chunk.js");
@@ -80,6 +81,8 @@ function preloadFlex(){
     }else{
         loadCss('/_content/Siesa.SDK.Frontend/flex/static/css/2.css?v=20230213');
         loadCss('/_content/Siesa.SDK.Frontend/flex/static/css/main.css?v=20230213');
+
+        loadScript('/_content/Siesa.SDK.Frontend/flex/FlexComponent.js?v=20230213');
 
         loadScript("/_content/Siesa.SDK.Frontend/flex/static/js/2.chunk.js?v=20230213");
         loadScript("/_content/Siesa.SDK.Frontend/flex/static/js/main.chunk.js?v=20230213");
