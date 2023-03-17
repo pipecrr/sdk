73d147b09921d4cc8771e8c4b8c9ff7e4a0cf4bc
arnol.meneses <arnol.meneses@siesa.com>
2023-03-17 18:06:36 -0500

    se modifican dtos

---
+++ Siesa.SDK.Shared/DTOS/SDKFlexDeltaColumnDTO.cs
@@ -0,0 +1,9 @@
+namespace Siesa.SDK.Shared.DTOS
+{
+    public class SDKFlexDeltaColumnDTO
+    {
+        public SDKFlexColumn Column { get; set; }
+        public string Action { get; set; }
+        public int Position { get; set; }
+    }
+}
--- Siesa.SDK.Shared/DTOS/SDKFlexDeltaDTO.cs
+++ Siesa.SDK.Shared/DTOS/SDKFlexDeltaDTO.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
+
 namespace Siesa.SDK.Shared.DTOS
 {
     public class SDKFlexDeltaDTO
     {
-        public SDKFlexColumn Column { get; set; }
-        public string Action { get; set; }
-        public int Position { get; set; }
+        public List<SDKFlexDeltaColumnDTO> Columns { get; set; }
+        public List<SDKFlexFilters> Filters { get; set; }
+        public List<SDKFlexColumn> Groups { get; set; }
     }
 }
