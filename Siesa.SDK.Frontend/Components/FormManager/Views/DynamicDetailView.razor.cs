bbae64c65c6327c0eb2ff73833bda82a6f479a8c
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-03-28 18:25:10 -0500

    init

--- Siesa.SDK.Frontend/Components/FormManager/Views/DynamicDetailView.razor.cs
+++ Siesa.SDK.Frontend/Components/FormManager/Views/DynamicDetailView.razor.cs
@@ -1,16 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
 using Siesa.SDK.Business;
+using Siesa.SDK.Frontend.Components.FormManager.Model;
 using Siesa.SDK.Frontend.Components.FormManager.ViewModels;
 using Siesa.SDK.Frontend.Extension;
+using Siesa.SDK.Frontend.Services;
+using Siesa.SDK.Shared.Services;
 
 namespace Siesa.SDK.Frontend.Components.FormManager.Views
 {
     public partial class DynamicDetailView : DynamicBaseViewModel
     {
+        [Inject]
+        public IBackendRouterService BackendRouterService { get; set; }
+        private FormViewModel FormViewModel { get; set; } = new FormViewModel();
+
+        private List<string> _extraFields = new List<string>();
+        private async Task GetExtraFields(string bName = null)
+        {
+            string _viewdefName = "detail";     
+
+            var metadata = BackendRouterService.GetViewdef(bName, _viewdefName);
+
+            if(String.IsNullOrEmpty(metadata))
+            {
+                metadata = BackendRouterService.GetViewdef(bName, "default");
+            }
+            if (String.IsNullOrEmpty(metadata))
+            {
+                //ErrorMsg = "No hay definición para la vista de detalle";
+                ErrorMsg = "Custom.Generic.ViewdefNotFound";
+            }
+            else
+            {
+                try
+                {
+                    FormViewModel = JsonConvert.DeserializeObject<FormViewModel>(metadata);
+
+                    if (FormViewModel.ExtraFields.Count > 0)
+                    {   
+                        var defaultFields = FormViewModel.Panels.SelectMany(panel => panel.Fields)
+                                        .Select(field => field.Name)
+                                        .Distinct()
+                                        .ToList(); 
+
+                        _extraFields =  FormViewModel.ExtraFields.Select(f => f.Name)                        
+                        .Union(defaultFields)
+                        .ToList();
+                        //_extraFields = _extraFields.Union(defaultFields).ToList();
+                    }
+                }
+                catch (System.Exception)
+                {
+                    //Soporte a viewdefs anteriores
+                    // var panels = JsonConvert.DeserializeObject<List<Panel>>(metadata);
+                    // FormViewModel.Panels = panels;
+                }
+            }
+        }
+
         private async Task InitDetail(Int64 business_obj_id)
         {
+            await GetExtraFields(BusinessName);
             try
             {
                 await BusinessObj.InitializeBusiness(business_obj_id);
