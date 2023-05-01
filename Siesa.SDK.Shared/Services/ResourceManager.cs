f82152b1ed36d9d63a52c937706f010f604bf47d
Jonathan Toledo <jtoledo@siesa.com>
2023-05-01 14:19:35 -0500

    login dto

--- Siesa.SDK.Shared/Services/ResourceManager.cs
+++ Siesa.SDK.Shared/Services/ResourceManager.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Siesa.SDK.Shared.Services;
 using Siesa.SDK.Shared.Json;
-using Siesa.SDK.Shared.Services;
 
 namespace Siesa.SDK.Shared.Services
 {
