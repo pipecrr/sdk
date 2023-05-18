6485bc2c767af42543da11fb7ca5ffe42a5ec7d4
arnol.meneses <arnol.meneses@siesa.com>
2023-05-18 10:57:53 -0500

    se corrigen problemas con selectRow de la grilla se aumenta version react se agrega mensaje error cuando no puede traer recursos

--- Siesa.SDK.Frontend/Components/FormManager/Views/ListView.razor.cs
+++ Siesa.SDK.Frontend/Components/FormManager/Views/ListView.razor.cs
@@ -958,8 +958,21 @@ namespace Siesa.SDK.Frontend.Components.FormManager.Views
             if(string.IsNullOrEmpty(item)){
                 return;
             }
-            IList<object> objects = JsonConvert.DeserializeObject<IList<object>>(item);
-            OnSelectionChanged(objects);
+            List<dynamic> objects = JsonConvert.DeserializeObject<List<dynamic>>(item);
+            IList<dynamic> list = new List<dynamic>();
+            foreach (var dynamicObj in objects)
+            {
+                dynamic obj = Activator.CreateInstance(BusinessObj.BaseObj.GetType());
+                foreach(var prop in dynamicObj){
+                    var propertyName = prop.Name;
+                    if(propertyName.Equals("rowid")){
+                        propertyName = "Rowid";
+                    }
+                    await SetValueObj(obj, propertyName, prop.Value);
+                }
+                list.Add(obj);
+            }
+            OnSelectionChanged(list);
         }
 
         [JSInvokable]
