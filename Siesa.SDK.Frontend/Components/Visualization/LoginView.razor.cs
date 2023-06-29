5078ccac24bfc31d7b50f0fa6e1e280e42ef9c52
arnol.meneses <arnol.meneses@siesa.com>
2023-06-29 10:10:58 -0500

    ajustes diseño y parametros componente portalLogin

--- Siesa.SDK.Frontend/Components/Visualization/LoginView.razor.cs
+++ Siesa.SDK.Frontend/Components/Visualization/LoginView.razor.cs
@@ -1,17 +1,20 @@
 using System;
+using Microsoft.AspNetCore.Components;
 using Siesa.SDK.Shared.DTOS;
-
 
 namespace Siesa.SDK.Frontend.Components.Visualization;
 
 public partial class LoginView
 {
+    [Parameter] public string LogoUrl { get; set; }
+    [Parameter] public string ImageUrl { get; set; }
+    [Parameter] public int RowidPortal { get; set; }
+    [Inject] public NavigationManager NavigationManager { get; set; }
     public int RowidCulture = 1;
     public string FormID { get; set; } = Guid.NewGuid().ToString();
     private SDKLoginModelDTO model = new SDKLoginModelDTO();
     private bool loading;
     private bool init_loading = true;
-    private string LogoUrl { get; set; } = "";
     private bool RecoveringPassword { get; set; } = false;
     private async void HandleValidSubmit(){
 
@@ -19,7 +22,6 @@ public partial class LoginView
     private async void HandleInvalidSubmit(){
         
     }
-
     private string GetCssLoding()
     {
         if (!init_loading)
