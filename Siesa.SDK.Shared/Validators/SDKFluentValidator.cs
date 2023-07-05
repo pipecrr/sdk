01123f88fccb382db3d6df590dbbd262352c1652
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-07-05 12:05:05 -0500

    .

--- Siesa.SDK.Shared/Validators/SDKFluentValidator.cs
+++ Siesa.SDK.Shared/Validators/SDKFluentValidator.cs
@@ -17,10 +17,34 @@ namespace Siesa.SDK.Shared.Validators
 
             foreach (var resultFluent in result.Errors)
             {
+
+                string ValueMessage = resultFluent.ErrorMessage;
+
+                if (resultFluent.ErrorMessage.Split("//").Count() > 1)
+                {
+                    var splitMessage = ValueMessage.Split("//");
+
+                    ValueMessage = $"{splitMessage[0]}//{BaseObject.GetType().Name}.{splitMessage[1]}";
+                    
+                }else if (!String.IsNullOrEmpty(resultFluent.PropertyName))
+                {
+
+                    ValueMessage = $"{resultFluent.ErrorMessage}//{BaseObject.GetType().Name}.{resultFluent.PropertyName}";
+
+                }
+                // else if (resultFluent.ErrorMessage.Split("//").Count() > 1)
+                // {
+                //     var splitMessage = ValueMessage.Split("//");
+
+                //     ValueMessage = $"{splitMessage[0]}//{BaseObject.GetType().Name}.{splitMessage[1]}";
+                // }
+
                 operationResult.Errors.Add(new OperationError
                 {
                     Attribute = (validator.ValidatorType != "" ? validator.ValidatorType + "." : "") + resultFluent.PropertyName,
-                    Message = resultFluent.ErrorMessage
+
+                   
+                    Message = ValueMessage
                 });
             }
 
