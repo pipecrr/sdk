9557262790f43bcf5f528eee041d4ec43b681f58
LeinerViloria <vilorialeiner2002@gmail.com>
2023-07-12 07:37:43 -0500

    Ajuste - llaves namespace

--- Siesa.SDK.Shared/DTOS/SDKActionInteractionsDTO.cs
+++ Siesa.SDK.Shared/DTOS/SDKActionInteractionsDTO.cs
@@ -1,26 +1,27 @@
 using System.Collections.Generic;
 
-namespace Siesa.SDK.Shared.DTOS;
-
-public abstract record SDKInteractionBase
+namespace Siesa.SDK.Shared.DTOS
 {
-    //Interaccion basada por accion
-    public SDKActionDTO Action { get; set; }
-    //Interaccion basada para acciones
-    public List<SDKActionDTO> Actions { get; set; }
-}
+    public abstract record SDKInteractionBase
+    {
+        //Interaccion basada por accion
+        public SDKActionDTO Action { get; set; }
+        //Interaccion basada para acciones
+        public List<SDKActionDTO> Actions { get; set; }
+    }
 
-//Una vez se crea, no cambia
-public record SDKActionInteractionsDTO : SDKInteractionBase
-{
-    public bool AnyActionCanAlter { get; set; }
-    public List<SDKInteractionDetail> ActionsToAlter { get; set; }
-}
+    //Una vez se crea, no cambia
+    public record SDKActionInteractionsDTO : SDKInteractionBase
+    {
+        public bool AnyActionCanAlter { get; set; }
+        public List<SDKInteractionDetail> ActionsToAlter { get; set; }
+    }
 
-//Una vez se crea, no cambia
-public record SDKInteractionDetail : SDKInteractionBase
-{
-    //Con qué estado va a interactuar, cuando prenda o cuando apague
-    public bool StatusToInteract { get; set; }
-    public bool ApplyToAll {get; set;}
+    //Una vez se crea, no cambia
+    public record SDKInteractionDetail : SDKInteractionBase
+    {
+        //Con qué estado va a interactuar, cuando prenda o cuando apague
+        public bool StatusToInteract { get; set; }
+        public bool ApplyToAll {get; set;}
+    }
 }
