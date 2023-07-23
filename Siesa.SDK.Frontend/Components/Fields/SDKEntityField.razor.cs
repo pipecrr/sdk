19ca722cbf5b141b4914deb7f6d2e2117a73cea2
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-07-23 15:49:52 -0500

    Ajuste:
    Se agrega parametro en SDKEnitityField - ValueChange Action<Object>
    Cambio de grupo cia  SDKEntityField

--- Siesa.SDK.Frontend/Components/Fields/SDKEntityField.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKEntityField.razor.cs
@@ -38,6 +38,7 @@ namespace Siesa.SDK.Frontend.Components.Fields
         [Parameter] public RelatedParams RelatedParams { get; set; }
         [Parameter] public Action<object> SetValue { get; set; }
         [Parameter] public Action OnChange { get; set; }
+        [Parameter] public Action<object> ValueChange { get; set; }
         [Parameter] public Action<List<dynamic>> OnReady { get; set; }
         [Parameter] public bool IsMultiple { get; set; } = false;
         [Parameter] public bool Disabled { get; set; }
@@ -173,12 +174,20 @@ namespace Siesa.SDK.Frontend.Components.Fields
         //     }
         // }
 
-        private async Task OnSelectItem(dynamic item){
+        private async Task OnSelectItem(dynamic item)
+        {
             SetVal(item);
-            if(OnChange != null){
+            if(OnChange != null)
+            {
                 OnChange();
+
+            }else if(ValueChange != null)
+            {
+                ValueChange(item);
             }
+
             LoadData("", null, true);
+            
             StateHasChanged();
         }
 
