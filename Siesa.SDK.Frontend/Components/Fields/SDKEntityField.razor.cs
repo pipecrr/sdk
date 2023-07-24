d69ca4065852c7432fd5ca9f4b9e06792430baff
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-07-24 10:19:06 -0500

    Clean SDKEntityField

--- Siesa.SDK.Frontend/Components/Fields/SDKEntityField.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKEntityField.razor.cs
@@ -38,7 +38,6 @@ namespace Siesa.SDK.Frontend.Components.Fields
         [Parameter] public RelatedParams RelatedParams { get; set; }
         [Parameter] public Action<object> SetValue { get; set; }
         [Parameter] public Action OnChange { get; set; }
-        [Parameter] public Action<object> ValueChange { get; set; }
         [Parameter] public Action<List<dynamic>> OnReady { get; set; }
         [Parameter] public bool IsMultiple { get; set; } = false;
         [Parameter] public bool Disabled { get; set; }
@@ -181,11 +180,7 @@ namespace Siesa.SDK.Frontend.Components.Fields
             {
                 OnChange();
 
-            }else if(ValueChange != null)
-            {
-                ValueChange(item);
             }
-
             LoadData("", null, true);
             
             StateHasChanged();
