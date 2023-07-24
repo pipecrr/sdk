06da8185a644af8c7cc8d26098a58e8cb1e5a4ac
admdesarrollo <admdesarrollo@siesa.com>
2023-07-24 10:42:20 -0500

    CREACION E00219_UserAccessToken

---
+++ Siesa.SDK.Shared/Entities/E00219_UserAccessToken.cs
@@ -0,0 +1,53 @@
+using Newtonsoft.Json;
+using Siesa.SDK.Entities;
+using Siesa.SDK.Shared.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Siesa.SDK.Shared.Json;
+using Microsoft.EntityFrameworkCore;
+using Siesa.Global.Enums;
+
+
+
+namespace Siesa.SDK.Entities
+{
+	/// <summary>
+	/// Token de usuario para API
+	/// </summary>
+
+	public partial class E00219_UserAccessToken : BaseSDK<int>
+	{
+		[Key]
+		[SDKRequired]
+		public override int Rowid { get; set; }
+
+		[SDKRequired]
+		[SDKStringLength(20)]
+		public string Id { get; set; }
+
+		public DateOnly? ExpirationDate { get; set; }
+
+		public DateTime? LastUsedDate { get; set; }
+
+		[ForeignKey("User")]
+		[SDKRequired]
+		public int RoiwdUser { get; set; }
+
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[SDKRequired]
+		public DateTime CreationDate { get; set; }
+
+		[SDKSensitiveData]
+		[SDKRequired]
+		[SDKStringLength(40)]
+		public string Token { get; set; }
+
+
+		[SDKCheckRelationship]
+		[SDKRequired]
+		public virtual E00220_User User { get; set; }
+
+	}
+}
