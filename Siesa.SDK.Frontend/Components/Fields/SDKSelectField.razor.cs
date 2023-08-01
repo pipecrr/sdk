ae0a2968d0d4d88ef6a3664881c1750aa89b335f
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-08-01 18:15:15 -0500

    Soporte a Enums SDKSelectField

--- Siesa.SDK.Frontend/Components/Fields/SDKSelectField.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKSelectField.razor.cs
@@ -9,6 +9,7 @@ using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Data.ResponseModel;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Siesa.SDK.Shared.Services;
 
 namespace Siesa.SDK.Frontend.Components.Fields
 {
@@ -30,6 +31,23 @@ namespace Siesa.SDK.Frontend.Components.Fields
         [Parameter] public string ValueProperty { get; set; }
         [Parameter] public string FieldName { get; set; }
         [Parameter] public Action OnFocusOut { get; set; } = () => { };
+        [Parameter] public bool IsSearch { get; set; }
+        private IEnumerable<SDKEnumWrapper<ItemType>> _options {get; set;} 
+        private Type enumType { get; set; }
+
+
+
+        /*protected override async Task OnParametersSetAsync()
+        {
+            await GetEnumValues().ConfigureAwait(true);
+            await base.OnParametersSetAsync().ConfigureAwait(true);
+        }   */
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetEnumValues().ConfigureAwait(true);
+            await base.OnInitializedAsync().ConfigureAwait(true);
+        }     
 
         public void RefreshCurrentText()
         {
@@ -69,6 +87,57 @@ namespace Siesa.SDK.Frontend.Components.Fields
                OnFocusOut();
             }
         }
+        private async Task GetEnumValues()
+        {
+            enumType = typeof(ItemType);
+            
+            if (enumType.IsEnum || (enumType.IsGenericType && enumType.GetGenericTypeDefinition() == typeof(Nullable<>) && enumType.GetGenericArguments()[0].IsEnum))
+            {
+                
+                if (enumType.IsGenericType && enumType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    enumType = enumType.GetGenericArguments()[0];
+                }
+
+                Dictionary<byte, string> enumValues = await ResourceManager.GetEnumValues(enumType.Name, AuthenticationService.GetRoiwdCulture()).ConfigureAwait(true);
+
+                if (enumValues == null || enumValues.Count == 0)
+                {
+                    return;
+                }
+                _options = Enumerable.Empty<SDKEnumWrapper<ItemType>>();
+
+                if (IsSearch)
+                {
+                    var SelectAllName = await ResourceManager.GetResource("Custom.Enum.SelectAll", AuthenticationService).ConfigureAwait(true);
+                    _options = _options.Append(new SDKEnumWrapper<ItemType>
+                    {
+                        Type = (ItemType)Enum.Parse(enumType, (enumValues.Select(x => x.Key).Max() + 1).ToString()),
+                        DisplayText = SelectAllName
+                    });
+
+                    Value = _options.Select(x => x.Type).First();
+                }
+
+                foreach (var option in enumValues)
+                {
+                    _options = _options.Append(new SDKEnumWrapper<ItemType>
+                    {
+                        Type = (ItemType)Enum.Parse(enumType, option.Key.ToString()),
+                        DisplayText = option.Value
+                    });
+                }
+            }
+            else
+            {
+                _options = Options;
+            }
+
+            _options = _options.Distinct();
+
+            StateHasChanged();
+        }
+
     }
 
 }
