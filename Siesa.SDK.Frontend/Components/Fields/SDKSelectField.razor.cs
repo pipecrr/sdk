4cb1ec9160698c9afa9ddcf18a244fa54159bfc9
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-08-02 09:54:22 -0500

    Code Smells - Documentacion

--- Siesa.SDK.Frontend/Components/Fields/SDKSelectField.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKSelectField.razor.cs
@@ -10,51 +10,112 @@ using DevExtreme.AspNet.Data.ResponseModel;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Siesa.SDK.Shared.Services;
+using System.Globalization;
 
 namespace Siesa.SDK.Frontend.Components.Fields
 {
+    /// <summary>
+    /// Select Field component
+    /// </summary>
+    /// <typeparam name="ItemType">The type of the value.</typeparam>
     public partial class SDKSelectField<ItemType> : SDKComponent
     {
         private DxComboBox<SDKEnumWrapper<ItemType>, ItemType> _refField;
-        [Parameter] public Expression<Func<ItemType>> ValueExpression { get; set; }
-        [Parameter] public ItemType Value { get; set; } = default!;
-        [Parameter] public Action<ItemType> ValueChanged { get; set; } = (value) => { };
-        [Parameter] public RenderFragment? ChildContent { get; set; }
 
+        /// <summary>
+        /// Gets or sets the ValueExpression.
+        /// </summary>
+        [Parameter] public Expression<Func<ItemType>> ValueExpression { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the Value. Represents the Selected Value
+        /// </summary>
+        [Parameter] public ItemType Value { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the ValueChanged. Event when a new Value is selected
+        /// </summary>
+        [Parameter] public Action<ItemType> ValueChanged { get; set; } = (value) => { };
+
+        /// <summary>
+        /// Gets or sets the ChildContent. Represents the content of the component
+        /// 
+        /// </summary>
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Options. Represents the list of options
+        /// can be of type enum or SDKEnumWrapper
+        /// If it is of type Enum, it is only necessary to specify the type in ItemType
+        /// </summary>
         [Parameter] public IEnumerable<SDKEnumWrapper<ItemType>> Options { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Placeholder.
+        /// </summary>
         [Parameter] public string Placeholder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Disabled.
+        /// </summary>
         [Parameter] public bool Disabled { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the ReadOnly.
+        /// </summary>
         [Parameter] public bool ReadOnly { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Required.
+        /// </summary>
+
         [Parameter] public bool Required { get; set; }
+
+        /// <summary>
+        /// Gets or sets the CssClass.
+        /// </summary>
+
         [Parameter] public string CssClass { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TextProperty.
+        /// </summary>
         [Parameter] public string TextProperty { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value property.
+        /// </summary>
+        /// <value>The value property.</value>
         [Parameter] public string ValueProperty { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FieldName.
+        /// </summary>
         [Parameter] public string FieldName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the OnFocusOut.
+        /// </summary>
         [Parameter] public Action OnFocusOut { get; set; } = () => { };
+
+        /// <summary>
+        /// Gets or sets IsSearch.
+        /// Is true when the component is to be painted in a lookup view.
+        ///false for any of the other views
+        /// </summary>
         [Parameter] public bool IsSearch { get; set; }
-        private IEnumerable<SDKEnumWrapper<ItemType>> _options {get; set;} 
+
+        private IEnumerable<SDKEnumWrapper<ItemType>> _options { get; set; }
         private Type enumType { get; set; }
 
-
-
-        /*protected override async Task OnParametersSetAsync()
-        {
-            await GetEnumValues().ConfigureAwait(true);
-            await base.OnParametersSetAsync().ConfigureAwait(true);
-        }   */
-
+        /// <summary>
+        /// Constructor Component
+        /// </summary>
         protected override async Task OnInitializedAsync()
         {
             await GetEnumValues().ConfigureAwait(true);
             await base.OnInitializedAsync().ConfigureAwait(true);
-        }     
-
-        public void RefreshCurrentText()
-        {
-
-
         }
-
         private void onKeyDown(KeyboardEventArgs e)
         {
             if (e.Key == "Enter")
@@ -63,37 +124,33 @@ namespace Siesa.SDK.Frontend.Components.Fields
             }
         }
 
-        private async Task<LoadResult> GetData(DataSourceLoadOptionsBase options, CancellationToken cancellationToken)
-        {
-            return new LoadResult
-            {
-                data = Options,
-                totalCount = Options.Count()
-            };
-        }
 
+        /// <summary>
+        /// Gets the automation identifier.
+        /// </summary>
         protected override string GetAutomationId()
         {
-            if(string.IsNullOrEmpty(AutomationId))
+            if (string.IsNullOrEmpty(AutomationId))
             {
                 AutomationId = FieldName;
             }
             return base.GetAutomationId();
         }
 
-        private async Task _OnFocusOut()
+        private void _OnFocusOut()
         {
-            if(OnFocusOut != null){
-               OnFocusOut();
+            if (OnFocusOut != null)
+            {
+                OnFocusOut();
             }
         }
         private async Task GetEnumValues()
         {
             enumType = typeof(ItemType);
-            
+
             if (enumType.IsEnum || (enumType.IsGenericType && enumType.GetGenericTypeDefinition() == typeof(Nullable<>) && enumType.GetGenericArguments()[0].IsEnum))
             {
-                
+
                 if (enumType.IsGenericType && enumType.GetGenericTypeDefinition() == typeof(Nullable<>))
                 {
                     enumType = enumType.GetGenericArguments()[0];
@@ -112,7 +169,7 @@ namespace Siesa.SDK.Frontend.Components.Fields
                     var SelectAllName = await ResourceManager.GetResource("Custom.Enum.SelectAll", AuthenticationService).ConfigureAwait(true);
                     _options = _options.Append(new SDKEnumWrapper<ItemType>
                     {
-                        Type = (ItemType)Enum.Parse(enumType, (enumValues.Select(x => x.Key).Max() + 1).ToString()),
+                        Type = (ItemType)Enum.Parse(enumType, (enumValues.Select(x => x.Key).Max() + 1).ToString(CultureInfo.InvariantCulture)),
                         DisplayText = SelectAllName
                     });
 
@@ -123,7 +180,7 @@ namespace Siesa.SDK.Frontend.Components.Fields
                 {
                     _options = _options.Append(new SDKEnumWrapper<ItemType>
                     {
-                        Type = (ItemType)Enum.Parse(enumType, option.Key.ToString()),
+                        Type = (ItemType)Enum.Parse(enumType, option.Key.ToString(CultureInfo.InvariantCulture)),
                         DisplayText = option.Value
                     });
                 }
