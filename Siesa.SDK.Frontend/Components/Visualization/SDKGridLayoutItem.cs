05923b74ad3fd5ac44f7dca7cbb2422408c0f6dc
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-08-03 15:14:04 -0500

    Ajuste SDKGridLayout

---
+++ Siesa.SDK.Frontend/Components/Visualization/SDKGridLayoutItem.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Rendering;
+using DevExpress.Blazor;
+
+
+namespace Siesa.SDK.Frontend.Components.Visualization
+{
+    /// <summary>
+    /// SDKGridLayoutItem is a wrapper of the DxGridLayoutItem component.
+    /// </summary>
+    public class SDKGridLayoutItem : DxGridLayoutItem
+    {
+        
+    }
+    
+}
