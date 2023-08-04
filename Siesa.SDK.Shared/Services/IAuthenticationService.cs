cd5ab25dd16be2b1adef7fb3441939659b787f07
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-08-04 07:58:51 -0500

    Summary

--- Siesa.SDK.Shared/Services/IAuthenticationService.cs
+++ Siesa.SDK.Shared/Services/IAuthenticationService.cs
@@ -4,11 +4,13 @@ using Siesa.SDK.Shared.DTOS;
 
 namespace Siesa.SDK.Shared.Services
 {
-    public class SessionRol {
+    public class SessionRol
+    {
         public int Rowid { get; set; }
         public string Name { get; set; }
     }
-    public class JwtUserData {
+    public class JwtUserData
+    {
         public int Rowid { get; set; }
         public string Path { get; set; }
         public string PasswordRecoveryEmail { get; set; }
@@ -18,8 +20,8 @@ namespace Siesa.SDK.Shared.Services
         public short RowidCulture { get; set; }
 
         public List<SessionRol> Roles { get; set; } = new List<SessionRol>();
-        
-        public short RowidCompanyGroup { get; set; } 
+
+        public short RowidCompanyGroup { get; set; }
         public short RowIdDBConnection { get; set; }
 
         public Dictionary<string, List<int>> FeaturePermissions { get; set; } = new();
@@ -27,7 +29,7 @@ namespace Siesa.SDK.Shared.Services
 
         public bool IsAdministrator { get; set; }
         public int? RowidAttachmentUserProfilePicture { get; set; }
-        public string PortalName {get; set;}
+        public string PortalName { get; set; }
         public string IdExternalUser { get; set; }
         public int RowidExternalUser { get; set; }
         public long RowidMainRecord { get; set; }
@@ -36,47 +38,222 @@ namespace Siesa.SDK.Shared.Services
             return $"({Id}) - {Name}";
         }
     }
-    
+    /// <summary>
+    /// Represents the authentication service interface for user login, logout, and related operations.
+    /// </summary>
     public interface IAuthenticationService
     {
+        /// <summary>
+        /// Gets the user token.
+        /// </summary>
+
         string UserToken { get; }
+        /// <summary>
+        /// Gets the portal user token.
+        /// </summary> 
         string PortalUserToken { get; }
+
+        /// <summary>
+        /// Gets the current user data.
+        /// </summary>
         JwtUserData User { get; }
+
+        /// <summary>
+        /// Gets the portal user data.
+        /// </summary>
         JwtUserData PortalUser { get; }
+        /// <summary>
+        /// Gets or sets the identifier of the culture changed row.
+        /// </summary> 
         short RowidCultureChanged { get; set; }
 
+        /// <summary>
+        /// Initializes the authentication service.
+        /// </summary>
         Task Initialize();
+
+        /// <summary>
+        /// Logs in the user with the provided credentials.
+        /// </summary>
+        /// <param name="username">The username of the user.</param>
+        /// <param name="password">The password of the user.</param>
+        /// <param name="rowIdDBConnection">The identifier of the database connection.</param>
+        /// <param name="IsUpdateSession">Indicates if the session should be updated.</param>
+        /// <param name="rowIdCompanyGroup">The identifier of the company group.</param>
         Task Login(string username, string password, short rowIdDBConnection,
                      bool IsUpdateSession = false, short rowIdCompanyGroup = 1);
+
+        /// <summary>
+        /// Logs in the portal user with the provided credentials.
+        /// </summary>
+        /// <param name="username">The username of the portal user.</param>
+        /// <param name="password">The password of the portal user.</param>
+        /// <param name="rowIdDBConnection">The identifier of the database connection.</param>
+        /// <param name="IsUpdateSession">Indicates if the session should be updated.</param>
+        /// <param name="rowIdCompanyGroup">The identifier of the company group.</param>
         Task LoginPortal(string username, string password, short rowIdDBConnection, bool IsUpdateSession = false, short rowIdCompanyGroup = 1);
+
+        /// <summary>
+        /// Logs out the user.
+        /// </summary>
         Task Logout();
+
+        /// <summary>
+        /// Logs out the portal user.
+        /// </summary>
         Task LogoutPortal();
+
+        /// <summary>
+        /// Sets the authentication token.
+        /// </summary>
+        /// <param name="token">The authentication token.</param>
+        /// <param name="saveLocalStorage">Indicates if the token should be saved in local storage.</param>
         public Task SetToken(string token, bool saveLocalStorage = true);
+
+        /// <summary>
+        /// Sets the authentication token for portal user.
+        /// </summary>
+        /// <param name="token">The authentication token.</param>
+        /// <param name="saveLocalStorage">Indicates if the token should be saved in local storage.</param>
         public Task SetTokenPortal(string token, bool saveLocalStorage = true);
+
+        /// <summary>
+        /// Sets the custom rowid for the culture changed.
+        /// </summary>
+        /// <param name="rowid">The custom rowid for the culture changed.</param>
         Task SetCustomRowidCulture(short rowid);
+
+        /// <summary>
+        /// Gets the custom rowid for the culture changed.
+        /// </summary>
+        /// <returns>The custom rowid for the culture changed.</returns>
+
         short GetRoiwdCulture();
+        /// <summary>
+        /// Sets the rowid for the selected company group.
+        /// </summary>
+        /// <param name="rowid">The rowid for the selected company group.</param>
         Task SetRowidCompanyGroup(short rowid);
+
+        /// <summary>
+        /// Removes the custom rowid for the culture changed.
+        /// </summary>
         Task RemoveCustomRowidCulture();
 
+
+        /// <summary>
+        /// Gets the row ID for the selected company group.
+        /// </summary>
+        /// <returns>The row ID for the selected company group.</returns>
         short GetRowidCompanyGroup();
+
+        /// <summary>
+        /// Sets the selected database connection.
+        /// </summary>
+        /// <param name="selectedConnection">The selected database connection.</param>
         Task SetSelectedConnection(SDKDbConnection selectedConnection);
+
+        /// <summary>
+        /// Gets the selected database connection information.
+        /// </summary>
+        /// <returns>The <see cref="SDKDbConnection"/> representing the selected database connection.</returns>
         SDKDbConnection GetSelectedConnection();
+
+        /// <summary>
+        /// Gets the identifier of the selected suite.
+        /// </summary>
+        /// <returns>The identifier of the selected suite.</returns>
         int GetSelectedSuite();
+
+        /// <summary>
+        /// Sets the identifier of the selected suite.
+        /// </summary>
+        /// <param name="rowid">The identifier of the selected suite.</param>
         void SetSelectedSuite(int rowid);
+
+        /// <summary>
+        /// Gets the logo associated with the database connection.
+        /// </summary>
+        /// <returns>The logo associated with the database connection.</returns>
         string GetConnectionLogo();
+
+        /// <summary>
+        /// Gets the style associated with the database connection.
+        /// </summary>
+        /// <returns>The style associated with the database connection.</returns>
         string GetConnectionStyle();
+        /// <summary>
+        /// Checks if the current authentication token is valid.
+        /// </summary>
+        /// <returns>A boolean value indicating whether the authentication token is valid.</returns>
         Task<bool> IsValidToken();
+
+        /// <summary>
+        /// Method to send an email to the user with the password recovery link.
+        /// </summary>
+        /// <param name="email">The email of the user.</param>
+        /// <param name="isPortal">Indicates if the password recovery is for a portal user.</param>
         void ForgotPasswordAsync(string email, bool isPortal = false);
+
+        /// <summary>
+        /// Method to validate the user token for password recovery.
+        /// </summary>
+        /// <param name="userToken">The user token for password recovery.</param>
+        /// <param name="isPortal">Indicates if the password recovery is for a portal user.</param>
+        /// <returns>A boolean value indicating whether the user token is valid for password recovery.</returns>
         Task<bool> ValidateUserToken(string userToken, bool isPortal = false);
-        Task<bool> ChangePassword(string userToken,short rowIdDBConnection, string NewPassword, string ConfirmPassword, bool isPortal = false );
+
+        /// <summary>
+        /// Changes the password of a user with the specified recovery token.
+        /// </summary>
+        /// <param name="userToken">Recovery token of the user.</param>
+        /// <param name="rowIdDBConnection">Identifier of the database connection.</param>
+        /// <param name="NewPassword">New password to be assigned to the user.</param>
+        /// <param name="ConfirmPassword">Confirmation of the new password.</param>
+        /// <param name="isPortal">Indicates if the password change is for a portal user.</param>
+        /// <returns>A boolean value indicating whether the password change was successful.</returns>
+        Task<bool> ChangePassword(string userToken, short rowIdDBConnection, string NewPassword, string ConfirmPassword, bool isPortal = false);
+        /// <summary>
+        /// Renews the authentication token for the current user.
+        /// </summary>
         Task RenewToken();
+        /// <summary>
+        /// Gets the user's photo URL.
+        /// </summary>
+        /// <returns>The URL of the user's photo.</returns>
         string GetUserPhoto();
+        /// <summary>
+        /// Sets the user's photo data.
+        /// </summary>
+        /// <param name="data">The photo data to set.</param>
+        /// <param name="saveLocalStorage">Optional. Determines whether to save the photo data in local storage. Default is true.</param>
+
         Task SetUserPhoto(string data, bool saveLocalStorage = true);
+        
+        /// <summary>
+        /// Sets the user's preferences.
+        /// </summary>
+        /// <param name="preferences">The <see cref="UserPreferencesDTO"/> object representing the user's preferences.</param>
         Task SetPreferencesUser(UserPreferencesDTO preferences);
+        
+        /// <summary>
+        /// Gets the user's preferences.
+        /// </summary>
+        /// <returns>The <see cref="UserPreferencesDTO"/> object representing the user's preferences.</returns>
         UserPreferencesDTO GetPreferencesUser();
+        
+        /// <summary>
+        /// Gets the theme style for the current user.
+        /// </summary>
+        /// <returns>The theme style.</returns>
         string GetThemeStyle();
+        
+        /// <summary>
+        /// Logs in the user using the specified user access token and database connection.
+        /// </summary>
+        /// <param name="userAccesstoken">The user access token to use for login.</param>
+        /// <param name="rowidDBConnection">The row ID of the database connection to use for login.</param>
+        /// <returns>The authentication token as a string.</returns>
         Task<string> LoginSessionByToken(string userAccesstoken, short rowidDBConnection);
     }
-
-    
 }
