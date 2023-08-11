ba95b4c219c217988fbd0e1f3bba0a5811a6cd84
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-08-11 14:10:41 -0500

    .

---
+++ Siesa.SDK.Backend/Services/QueueServices.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Siesa.SDK.Backend.Services
+{
+    public class QueueService
+    {
+
+        public QueueService()
+        {
+              
+        }
+
+        public 
+
+    }
+}
