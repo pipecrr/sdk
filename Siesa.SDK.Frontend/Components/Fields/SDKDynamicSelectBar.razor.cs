267f7ee7de850683ac0e05ca0d03999bb400db2b
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-08-18 10:46:11 -0500

    Doc

--- Siesa.SDK.Frontend/Components/Fields/SDKDynamicSelectBar.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKDynamicSelectBar.razor.cs
@@ -8,15 +8,32 @@ using Siesa.SDK.Frontend.Components;
 
 namespace Siesa.SDK.Frontend.Components.Fields
 {
+    /// <summary>
+    /// Represents a dynamic select bar component that allows choosing from a list of options.
+    /// </summary>
     public partial class SDKDynamicSelectBar : SDKComponent
     {
-        [Parameter] public List<DynamicSelectBarDetailDTO> Items { get; set; }
-        [Parameter] public Action ValueChanged { get; set; }
-        [Parameter] public bool Disabled { get; set; }
+        /// <summary>
+        /// Gets or sets the list of items for the dynamic select bar.
+        /// </summary>
+        [Parameter]
+        public List<DynamicSelectBarDetailDTO> Items { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action to be invoked when the value changes.
+        /// </summary>
+        [Parameter]
+        public Action ValueChanged { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the dynamic select bar is disabled.
+        /// </summary>
+        [Parameter]
+        public bool Disabled { get; set; }
         private string _id = $"{Guid.NewGuid()}";
 
-        private string GetActiveCss(DynamicSelectBarDetailDTO Item) => !Item.On ? string.Empty : "rz-state-active";
-        private string GetStyle(DynamicSelectBarDetailDTO Item) => $"color: #{Item.IconColor};";
+        private static string GetActiveCss(DynamicSelectBarDetailDTO Item) => !Item.On ? string.Empty : "rz-state-active";
+        private static string GetStyle(DynamicSelectBarDetailDTO Item) => $"color: #{Item.IconColor};";
 
         private void OnChange(DynamicSelectBarDetailDTO Item)
         {
