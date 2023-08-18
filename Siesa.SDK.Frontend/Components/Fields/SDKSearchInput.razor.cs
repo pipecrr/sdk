fd1a1385829c8f44356722f6593ae805ba0871e3
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-08-18 17:40:55 -0500

    Summary

--- Siesa.SDK.Frontend/Components/Fields/SDKSearchInput.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKSearchInput.razor.cs
@@ -5,36 +5,62 @@ using System.Threading.Tasks;
 
 namespace Siesa.SDK.Frontend.Components.Fields
 {
+    /// <summary>
+    /// Represents  an component with input field for searching 
+    /// </summary>
     public partial class SDKSearchInput : SDKComponent
     {
+
+        /// <summary>
+        /// Gets or sets the current value of the search input.
+        /// </summary>
 
         [Parameter]
         public string Value { get; set; }
 
+        /// <summary>
+        /// Gets or sets the placeholder text displayed inside the search input when it's empty.
+        /// </summary>
         [Parameter]
         public string Placeholder { get; set; } = "Search...";
 
+        /// <summary>
+        /// Gets or sets the callback that is invoked when the value of the search input changes.
+        /// </summary>
         [Parameter]
         public EventCallback<string> ValueChanged { get; set; }
 
+        /// <summary>
+        /// Gets or sets the minimum time in milliseconds between consecutive search requests.
+        /// </summary>
         [Parameter]
         public int MinMillisecondsBetweenSearch { get; set; } = 250;
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the search input is disabled.
+        /// </summary>
         [Parameter]
         public bool Disabled { get; set; }
 
+        /// <summary>
+        /// Gets or sets the minimum number of characters required to trigger a search.
+        /// </summary>
         [Parameter]
         public int MinToFilter { get; set; } = 1;
 
+        /// <summary>
+        /// Gets or sets the CSS class to apply to the search input.
+        /// </summary>
         [Parameter]
         public string CssClass { get; set; }
-        private CancellationTokenSource _cancellationToken {get; set;}
+
+        private CancellationTokenSource _cancellationToken { get; set; }
 
         private async Task HandleInput(ChangeEventArgs e)
         {
             Value = e.Value.ToString().Trim();
 
-            if(_cancellationToken is not null)
+            if (_cancellationToken is not null)
             {
                 _cancellationToken.Cancel();
             }
@@ -44,13 +70,13 @@ namespace Siesa.SDK.Frontend.Components.Fields
             var token = _cancellationToken.Token;
 
             await Task.Delay(MinMillisecondsBetweenSearch, token).ConfigureAwait(true);
-            
+
             if (token.IsCancellationRequested)
                 return;
 
-            if(!string.IsNullOrEmpty(Value) && Value.Length < MinToFilter)
+            if (!string.IsNullOrEmpty(Value) && Value.Length < MinToFilter)
                 return;
-            
+
             await ValueChanged.InvokeAsync(Value).ConfigureAwait(true);
         }
 
