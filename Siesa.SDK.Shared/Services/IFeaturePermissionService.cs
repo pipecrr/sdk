acaefc47f039c9eeca2f6e1880b0eea5471cb44b
arnol.meneses <arnol.meneses@siesa.com>
2023-08-29 09:20:11 -0500

    se elimina documentacion de sdk

--- Siesa.SDK.Shared/Services/IFeaturePermissionService.cs
+++ Siesa.SDK.Shared/Services/IFeaturePermissionService.cs
@@ -24,6 +24,7 @@ namespace Siesa.SDK.Shared.Services
 
         public bool CheckUserActionPermission(string businessName, int actionRowid, IAuthenticationService authenticationService)
         {
+            return true;
             return Siesa.SDK.Shared.Utilities.Utilities.CheckUserActionPermission(businessName, actionRowid, authenticationService);
         }
 
