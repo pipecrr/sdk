157237d7aa7fcc3188fee71f4a4189bb00cd9b5e
arnol.meneses <arnol.meneses@siesa.com>
2023-09-04 10:16:07 -0500

    se evalua codigo que viene desde viewdef en control entidad

--- Siesa.SDK.Frontend/Components/FormManager/Fields/EntityField.razor.cs
+++ Siesa.SDK.Frontend/Components/FormManager/Fields/EntityField.razor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
 using Siesa.SDK.Frontend.Components.FormManager.Model.Fields;
 using Siesa.SDK.Frontend.Components.FormManager.ViewModels;
 using Siesa.SDK.Frontend.Utils;
@@ -16,6 +17,7 @@ namespace Siesa.SDK.Frontend.Components.FormManager.Fields
         public string RelatedBusiness { get; set; }
         public Action<List<dynamic>> OnReady { get; set; }
         private Dictionary<string, string> _relatedFilters = new Dictionary<string, string>();
+        private List<List<object>> _filters = new List<List<object>>(); 
 
         protected override async Task OnInitializedAsync()
         {
@@ -34,6 +36,40 @@ namespace Siesa.SDK.Frontend.Components.FormManager.Fields
                     var dynamicValue = await Evaluator.EvaluateCode(filter.Value, BaseModelObj);
                     _relatedFilters.Add(filter.Key, dynamicValue.ToString());
                 }
+            }
+            
+            if(FieldOpt.Filters != null){
+                EvaluateFilters();
+            }
+        }
+
+        private void EvaluateFilters()
+        {
+            foreach(var filter in FieldOpt.Filters){
+                List<object> filtersInside = new List<object>();
+                foreach(var item in filter)
+                {
+                    var properties = JsonConvert.DeserializeObject<dynamic>(item.ToString()).Properties();
+                    bool nullvalue = true;
+                    foreach (var property in properties)
+                    {
+                        var name = property.Name;
+                        var codeValue = property.Value.ToString();
+                        var dynamicValue = Evaluator.EvaluateCode(codeValue, BaseModelObj);
+                        try
+                        {
+                            item.GetType().GetProperty(name)?.SetValue(item, dynamicValue);
+                            nullvalue = false;
+                        }catch(NullReferenceException ex){
+                            Console.WriteLine(ex.Message);
+                            nullvalue = true;
+                        }
+                    }
+                    if(!nullvalue)
+                        filtersInside.Add(item);
+                }
+                if(filtersInside.Count > 0)
+                    _filters.Add(filtersInside);
             }
         }
 
