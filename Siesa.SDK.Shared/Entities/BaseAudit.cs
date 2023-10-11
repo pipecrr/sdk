328df45826456558d1cc42f6c9388a1c15dd5b5a
Jonathan Toledo <jtoledo@siesa.com>
2023-10-11 19:50:17 +0000

    fix gestor

--- Siesa.SDK.Shared/Entities/BaseAudit.cs
+++ Siesa.SDK.Shared/Entities/BaseAudit.cs
@@ -22,11 +22,15 @@ namespace Siesa.SDK.Entities
 		public virtual string? Source { get; set; }
 
 		[SDKRequired]
+		[ForeignKey("UserCreates")]
 		public virtual int RowidUserCreates { get; set; }
-
+		[ForeignKey("UserLastUpdate")]
 		public virtual int? RowidUserLastUpdate { get; set; }
 
 		public virtual int? RowidSession { get; set; }
+
+		public virtual E00220_User UserCreates { get; set; }
+		public virtual E00220_User? UserLastUpdate { get; set; }
 
 
 	}
