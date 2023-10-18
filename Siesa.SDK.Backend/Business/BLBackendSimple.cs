30d1101e1ee785b64763b9e07d4eafa5ab54e951
arnol.meneses <arnol.meneses@siesa.com>
2023-10-18 11:50:51 -0500

    ajuste sonar

--- Siesa.SDK.Backend/Business/BLBackendSimple.cs
+++ Siesa.SDK.Backend/Business/BLBackendSimple.cs
@@ -1072,7 +1072,7 @@ namespace Siesa.SDK.Business
         protected virtual void DeleteVisibilityEntity(SDKContext context)
         {
             var nameSpaceEntity = typeof(T).Namespace;
-            var nameVisibilityEntity = "U" + typeof(T).Name.Substring(1);
+            var nameVisibilityEntity = "U" + string.Concat(typeof(T).Name.AsSpan(1).ToString());
             var visibilityEntityType = Utilities.SearchType(nameSpaceEntity + "." + nameVisibilityEntity, true);
             if (visibilityEntityType != null)
             {
