32ae8e0b2f65518e9d428dfe57cfec337a3e7b1f
FelipeCortes <fcortesc@siesa.com>
2023-11-02 11:26:48 -0500

    NewParameters

--- Siesa.SDK.Frontend/Components/Visualization/SDKTree.razor.cs
+++ Siesa.SDK.Frontend/Components/Visualization/SDKTree.razor.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Components;
 using Radzen;
 
@@ -5,18 +8,33 @@ namespace Siesa.SDK.Frontend.Components.Visualization;
 public partial class SDKTree : SDKComponent
 {
     [Parameter]
-    public EventCallback<TreeEventArgs> Change { get; set; }
+    public EventCallback<TreeEventArgs> OnChange { get; set; }
     [Parameter]
-    public EventCallback<TreeExpandEventArgs> Expand { get; set; }
+    public EventCallback<TreeExpandEventArgs> EventExpand { get; set; }
     [Parameter]
-    public EventCallback<TreeEventArgs> Collapse { get; set; }
+    public EventCallback<TreeEventArgs> EventCollapse { get; set; }
     [Parameter]
     public RenderFragment ChildContent { get; set; }
     [Parameter]
     public bool SingleExpand { get; set; }
-
     [Parameter]
-    public string Style {get; set;}
+    public IEnumerable Data { get; set; }
+    [Parameter]
+    public object Value { get; set; }
+    [Parameter]
+    public EventCallback<object> ValueChanged { get; set; }
+    [Parameter]
+    public bool AllowCheckBoxes { get; set; }
+    [Parameter]
+    public bool AllowCheckChildren { get; set; } = true;
+    [Parameter]
+    public bool AllowCheckParents { get; set; } = true;
+    [Parameter]
+    public IEnumerable<object> CheckedValues { get; set; } = Enumerable.Empty<object>();
+    [Parameter]
+    public EventCallback<IEnumerable<object>> CheckedValuesChanged { get; set; }
+    [Parameter]
+    public string Style { get; set; }
 
 
 }
