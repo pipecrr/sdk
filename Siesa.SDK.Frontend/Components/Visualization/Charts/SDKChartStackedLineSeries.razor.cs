2725e7b6aa66efe95c5ac351a1576d847ea1be52
FelipeCortes <fcortesc@siesa.com>
2023-11-17 15:12:01 -0500

    SDKChartStackedLineSeries

--- Siesa.SDK.Frontend/Components/Visualization/Charts/SDKChartStackedLineSeries.razor.cs
+++ Siesa.SDK.Frontend/Components/Visualization/Charts/SDKChartStackedLineSeries.razor.cs
@@ -21,37 +21,38 @@ public partial class SDKChartStackedLineSeries<TData, TArgument, TValue> : SDKCo
 {
     [Parameter]
     public IEnumerable<TData> Data { get; set; }
+
     [Parameter]
     public Expression<Func<TData, TArgument>> ArgumentField { get; set; }
+
     [Parameter]
     public ChartSeriesSettings<TData, TValue, TArgument> Settings { get; set; }
+
     [Parameter]
     public Expression<Func<TData, TValue>> ValueField { get; set; }
+
     [Parameter]
     public string Axis { get; set; }
     [Parameter]
     public ChartContinuousSeriesHoverMode HoverMode { get; set; }
     [Parameter]
-    public double Opacity { get; set; } = 0.5;
-    [Parameter]
     public ChartContinuousSeriesSelectionMode SelectionMode { get; set; }
+
     [Parameter]
     public bool BreakOnEmptyPoints { get; set; }
+
     [Parameter]
     public Color Color { get; set; }
+
     [Parameter]
     public Expression<Func<TData, bool>> Filter { get; set; }
+
     [Parameter]
     public string Pane { get; set; }
-    [Parameter]
-    public int Width { get; set; }= 2;
+
     [Parameter]
     public string Name { get; set; }
-    [Parameter]
-    public bool Visible { get; set; }
-    [Parameter]
-    public Func<IEnumerable<TValue>, TValue> SummaryMethod { get; set; }
-    [Parameter] 
-    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter] public RenderFragment? ChildContent { get; set; }
 
 }
