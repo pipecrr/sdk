4cff4501c753651cf036c5695b1ea57c651c9fbb
DavidGrisalesSiesa <idgrisalesb@siesa.com>
2023-11-20 14:04:36 -0500

    Ajustes SelectBar

--- Siesa.SDK.Frontend/Components/Fields/SDKSelectBar.razor.cs
+++ Siesa.SDK.Frontend/Components/Fields/SDKSelectBar.razor.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components;
+using Siesa.SDK.Frontend.Components.FormManager.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 
 namespace Siesa.SDK.Frontend.Components.Fields
@@ -15,6 +18,54 @@ namespace Siesa.SDK.Frontend.Components.Fields
         [Parameter] public Action<ItemType> ValueChanged {get; set;}
 
         [Parameter] public bool Disabled { get; set; }
+        [Parameter] public Expression<Func<ItemType>> ValueExpression { get; set; }
+        [Parameter] public string FieldName { get; set; }
+
+        [Parameter] public string DenialResourceTag {get; set;}
+        [Parameter] public string AffirmationResourceTag {get; set;}
+
+        [Inject] 
+        private UtilsManager UtilManager {get; set;}
+
+        private string DenialText {get; set;}
+        private string AffirmationText {get; set;}
+
+         protected override string GetAutomationId()
+        {
+            if(string.IsNullOrEmpty(AutomationId))
+            {
+                if (!string.IsNullOrEmpty(FieldName))
+                {
+                        AutomationId = FieldName;
+                }
+            }
+            return base.GetAutomationId();
+        }
+
+        private async Task GetResources()
+        {
+            if(typeof(ItemType) == typeof(bool))
+            {   
+                DenialResourceTag = string.IsNullOrEmpty(DenialResourceTag) ? "Custom.Selectbar.Boolean.No" : DenialResourceTag;
+                AffirmationResourceTag = string.IsNullOrEmpty(AffirmationResourceTag) ? "Custom.Selectbar.Boolean.Yes" : AffirmationResourceTag;
+
+                DenialText = await UtilManager.GetResource(DenialResourceTag);
+                AffirmationText = await UtilManager.GetResource(AffirmationResourceTag);
+                StateHasChanged();
+            }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            await GetResources();
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            base.OnParametersSet();
+            await GetResources();
+        }
 
     }
 }
