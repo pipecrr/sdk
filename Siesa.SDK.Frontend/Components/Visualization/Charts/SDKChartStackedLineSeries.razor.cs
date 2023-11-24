cfb695700f506c186d7f08df2723572bfb2321d1
FelipeCortes <fcortesc@siesa.com>
2023-11-24 13:25:07 -0500

    sdk

--- Siesa.SDK.Frontend/Components/Visualization/Charts/SDKChartStackedLineSeries.razor.cs
+++ Siesa.SDK.Frontend/Components/Visualization/Charts/SDKChartStackedLineSeries.razor.cs
@@ -19,40 +19,55 @@ namespace Siesa.SDK.Frontend.Components.Visualization.Charts;
 
 public partial class SDKChartStackedLineSeries<TData, TArgument, TValue> : SDKComponent
 {
+    // Colección de datos para la serie del gráfico.
     [Parameter]
     public IEnumerable<TData> Data { get; set; }
 
+    // Expresión que representa el campo de argumentos de la serie del gráfico.
     [Parameter]
     public Expression<Func<TData, TArgument>> ArgumentField { get; set; }
 
+    // Configuración específica de la serie del gráfico.
     [Parameter]
     public ChartSeriesSettings<TData, TValue, TArgument> Settings { get; set; }
 
+    // Expresión que representa el campo de valores de la serie del gráfico.
     [Parameter]
     public Expression<Func<TData, TValue>> ValueField { get; set; }
 
+    // Define el eje asociado a la serie del gráfico.
     [Parameter]
     public string Axis { get; set; }
+
+    // Modo de visualización al pasar el mouse sobre la serie del gráfico.
     [Parameter]
     public ChartContinuousSeriesHoverMode HoverMode { get; set; }
+
+    // Modo de selección de la serie del gráfico.
     [Parameter]
     public ChartContinuousSeriesSelectionMode SelectionMode { get; set; }
 
+    // Determina si se debe interrumpir en puntos vacíos.
     [Parameter]
     public bool BreakOnEmptyPoints { get; set; }
 
+    // Color de la serie del gráfico.
     [Parameter]
     public Color Color { get; set; }
 
+    // Expresión para filtrar los datos de la serie del gráfico.
     [Parameter]
     public Expression<Func<TData, bool>> Filter { get; set; }
 
+    // Define el panel asociado a la serie del gráfico.
     [Parameter]
     public string Pane { get; set; }
 
+    // Nombre de la serie del gráfico.
     [Parameter]
     public string Name { get; set; }
 
-    [Parameter] public RenderFragment? ChildContent { get; set; }
-
+    // Contenido adicional que se puede incrustar en el componente.
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
 }
