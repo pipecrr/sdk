61defd285539523f2599182388e80bf21fd0173e
FelipeCortes <fcortesc@siesa.com>
2023-11-27 01:19:30 -0500

    SDKSummary

--- Siesa.SDK.Frontend/Components/Visualization/Charts/SDKChartFullStackedAreaSeries.razor.cs
+++ Siesa.SDK.Frontend/Components/Visualization/Charts/SDKChartFullStackedAreaSeries.razor.cs
@@ -8,45 +8,89 @@ using Siesa.SDK.Frontend.Components;
 
 
 namespace Siesa.SDK.Frontend.Components.Visualization.Charts;
-
 public partial class SDKChartFullStackedAreaSeries<TData, TArgument, TValue> : SDKComponent
 {
+    /// <summary>
+    /// Data to be displayed in the series.
+    /// </summary>
     [Parameter]
     public IEnumerable<TData> Data { get; set; }
 
+    /// <summary>
+    /// Defines the argument field for the series.
+    /// </summary>
     [Parameter]
     public Expression<Func<TData, TArgument>> ArgumentField { get; set; }
 
+    /// <summary>
+    /// Settings for the series.
+    /// </summary>
     [Parameter]
     public ChartSeriesSettings<TData, TValue, TArgument> Settings { get; set; }
 
+    /// <summary>
+    /// Defines the value field for the series.
+    /// </summary>
     [Parameter]
     public Expression<Func<TData, TValue>> ValueField { get; set; }
 
+    /// <summary>
+    /// Associated axis for the series.
+    /// </summary>
     [Parameter]
     public string Axis { get; set; }
+
+    /// <summary>
+    /// Hover mode for the series.
+    /// </summary>
     [Parameter]
     public ChartContinuousSeriesHoverMode HoverMode { get; set; }
+
+    /// <summary>
+    /// Opacity of the series. Default is 0.5.
+    /// </summary>
     [Parameter]
     public double Opacity { get; set; } = 0.5;
+
+    /// <summary>
+    /// Selection mode for the series.
+    /// </summary>
     [Parameter]
     public ChartContinuousSeriesSelectionMode SelectionMode { get; set; }
 
+    /// <summary>
+    /// Whether to break on empty points.
+    /// </summary>
     [Parameter]
     public bool BreakOnEmptyPoints { get; set; }
 
+    /// <summary>
+    /// Color of the series.
+    /// </summary>
     [Parameter]
     public Color Color { get; set; }
 
+    /// <summary>
+    /// Filtering expression for the series.
+    /// </summary>
     [Parameter]
     public Expression<Func<TData, bool>> Filter { get; set; }
 
+    /// <summary>
+    /// Associated pane for the series.
+    /// </summary>
     [Parameter]
     public string Pane { get; set; }
 
+    /// <summary>
+    /// Name of the series.
+    /// </summary>
     [Parameter]
     public string Name { get; set; }
 
-    [Parameter] public RenderFragment? ChildContent { get; set; }
-
+    /// <summary>
+    /// Content to be rendered within the series.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
 }
