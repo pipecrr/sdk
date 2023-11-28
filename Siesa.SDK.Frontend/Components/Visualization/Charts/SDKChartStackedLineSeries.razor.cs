48914965919a181410ad991e398358d9a3979eff
FelipeCortes <fcortesc@siesa.com>
2023-11-28 08:43:59 -0500

    Changes SDKChartContinuousSeriesHoverMode

--- Siesa.SDK.Frontend/Components/Visualization/Charts/SDKChartStackedLineSeries.razor.cs
+++ Siesa.SDK.Frontend/Components/Visualization/Charts/SDKChartStackedLineSeries.razor.cs
@@ -54,13 +54,13 @@ public partial class SDKChartStackedLineSeries<TData, TArgument, TValue> : SDKCo
     /// Mouse hover mode for the chart series.
     /// </summary>
     [Parameter]
-    public ChartContinuousSeriesHoverMode HoverMode { get; set; }
+    public SDKChartContinuousSeriesHoverMode HoverMode { get; set; }
 
     /// <summary>
     /// Selection mode for the chart series.
     /// </summary>
     [Parameter]
-    public ChartContinuousSeriesSelectionMode SelectionMode { get; set; }
+    public SDKChartContinuousSeriesSelectionMode SelectionMode { get; set; }
 
     /// <summary>
     /// Determines if it should break on empty points.
