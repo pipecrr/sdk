098d715c9db7b33bea6dd272fff06afceaeb6665
FelipeCortes <fcortesc@siesa.com>
2023-11-29 09:25:13 -0500

    Enums SDKStackedLine

--- Siesa.SDK.Frontend/Components/SDKEnums.cs
+++ Siesa.SDK.Frontend/Components/SDKEnums.cs
@@ -242,17 +242,17 @@ namespace Siesa.SDK.Frontend.Components
     }
     public enum SDKChartContinuousSeriesHoverMode 
     {
-    NearestPoint = 0,
-    SeriesAndAllPoints = 1,
-    Series = 2,
-    None = 3
+        NearestPoint = 0,
+        SeriesAndAllPoints = 1,
+        Series = 2,
+        None = 3
 
     }
 
     public enum SDKChartContinuousSeriesSelectionMode
     {
-    Single = 0,
-    Multiple = 1,
-    None = 2
+        Series = 0,
+        SeriesAndAllPoints = 1,
+        None = 2
     }
 }
